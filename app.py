ca19d10a83c56ca288f65971b567f511f137d688
ElvisAns <ansimapersic@gmail.com>
2022-08-01 01:24:30 +0200

    Created the todo model

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+class todo(db.Model):
+    __tablename__ = 'todo' #dont use uppercase letter and space
+    id =  db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(),nullable=False,unique=True)
+    description = db.Column(db.String(50),nullable=False)
+    completed = db.Column(db.Boolean,default=0)
+    def __repr__(self): #will handle printing more especially in debugging
+        return f'<Person : {self.title} , {self.description}> is it completed yet? {self.completed}'
