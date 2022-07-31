62b9bbcbbdd73c39fde906a62f15529ee21b6257
ElvisAns <ansimapersic@gmail.com>
2022-08-01 01:31:37 +0200

    fix typos in postgres connexion uri

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres@localhost:5432/testdb'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://root:root@localhost:5432/testdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
