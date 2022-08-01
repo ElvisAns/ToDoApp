6670e0b1cc15e89e5c8f050f6721df3198fbd3aa
ElvisAns <ansimapersic@gmail.com>
2022-08-01 14:05:49 +0200

    Basic handler for the form submission

--- static/script.js
+++ static/script.js
@@ -1,6 +1,30 @@
 function saveToDo(e){
     e.preventDefault()
-    alert("Processing")
+    let desc = document.getElementsById("task_desc").value
+    let title = document.getElementById("task_title").value
+
+    if(desc.length<5 || title.length<5){
+        alert("Veuillez completez les champs requis")
+        return
+    }
+
+    fetch("/save_to_do/",{
+        method:POST,
+        body: JSON.stringify({
+                'title' : title,
+                'description':desc
+            }
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(jsonResponse => {
+    })
+    .catch(function(){
+        alert("Une erreur s'est produite lors de l'enregistrement")
+    })
 }
 
 document.getElementById("formSave").addEventListener("submit",saveToDo)
